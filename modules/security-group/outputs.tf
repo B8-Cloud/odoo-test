0bf259cf1d9a5d2ba0ac736a1c51053ed2103d51
B8-Cloud <minn.b8cloud@gmail.com>
2025-10-20 22:38:29 +0630

    Update outputs.tf

--- modules/security-group/outputs.tf
+++ modules/security-group/outputs.tf
@@ -1,3 +1,14 @@
 output "sg_id" {
-  value = aws_security_group.this.id
+  value       = var.create_sg ? aws_security_group.this[0].id : null
+  description = "Security Group ID"
+}
+
+output "role_name" {
+  value       = var.create_iam_role ? aws_iam_role.this[0].name : null
+  description = "IAM Role Name"
+}
+
+output "instance_profile_name" {
+  value       = var.create_iam_role ? aws_iam_instance_profile.this[0].name : null
+  description = "IAM Instance Profile Name"
 }
