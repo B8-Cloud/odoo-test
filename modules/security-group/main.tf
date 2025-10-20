70976448830b372040e587b8f6ce0318fd093881
B8-Cloud <minn.b8cloud@gmail.com>
2025-10-20 22:25:14 +0630

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -1,1 +1,44 @@
+resource "aws_security_group" "this" {
+  name        = var.sg_name
+  description = "Security group"
+  vpc_id      = var.vpc_id
 
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidrs
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidrs
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidrs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_name
+  }
+}
+
+output "sg_id" {
+  value = aws_security_group.this.id
+}
