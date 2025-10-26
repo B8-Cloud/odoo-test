66d0869f98b54b0f62bcb84907c5c2eb399e1861
ShSwanz <swanhtetaung112112@gmail.com>
2025-10-26 09:38:05 +0630

    commit

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  description = "ECR repository name"
+  type        = string
+}
+
+variable "image_tag_mutability" {
+  description = "The tag mutability setting for the repository"
+  type        = string
+  default     = "MUTABLE"
+}
+
+variable "scan_on_push" {
+  description = "Indicates whether images are scanned after being pushed to the repository"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Tags for the ECR repository"
+  type        = map(string)
+  default     = {}
+}
+
